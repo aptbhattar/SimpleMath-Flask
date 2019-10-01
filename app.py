67fc044a137627d985c084da804e7df7c2a5f2b3
Arpit Bhattar <43109086+aptbhattar@users.noreply.github.com>
2019-10-02 02:26:44 +0530

    Revert "Going to calculate a^n"

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
 from routes.routes import router
-from flask.ext.wtf import Form
-from wtforms import IntegerField, SubmitField, validators, ValidationError
 
 PORT = 5000
 
@@ -13,8 +11,3 @@ app.register_blueprint(router, url_prefix="/math")
 
 if __name__ == '__main__':
     app.run(port=PORT)
-    
-class power(calc):
-    A = IntegerField("Enter Number: ")
-    B = IntegerField("Enter Power: ")
-    submit = SubmitField("Answer")
