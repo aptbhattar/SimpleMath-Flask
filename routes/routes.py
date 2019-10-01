681e475564b3f8c7cd07e837c4598e8f5dfe36f0
Arpit Bhattar <43109086+aptbhattar@users.noreply.github.com>
2019-10-02 02:23:02 +0530

    Going to calculate a^n
    commit 9354c4a

--- routes/routes.py
+++ routes/routes.py
@@ -8,18 +8,15 @@ def check():
 
 @router.route("/add", methods=["POST"])
 def add():
-    # Add logic here
-    return
-
-@app.route('/', methods=['GET', 'POST'])
-def home():
     calc = power()
-
     if request.method == 'POST': 
         for i in range(b):
             a=a*a
             i=i+1
-            print(a)
+        return(a)
 
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    
     elif request.method == 'GET':
         return render_template('home.html', form=form)
